0e2bc8bc213a3e904c9d42a71d53875e61830a84
4K7-Coder <78072738+4K7-Coder@users.noreply.github.com>
2021-02-05 15:08:54 +0600

    Add files via upload

---
+++ A C++ Program to find the product of two numbers entered by the User.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float a,b;
+  float p;
+  
+  cout <<"Product of two numbers in C++\n\n";
+  cout <<"Enter two numbers : ";
+  cin >> a >> b;
+  p=a*b;
+  
+  cout <<"\nThe product of "<< a << " and " << b << " is " << p <<"\n";
+  return (0);
+}
